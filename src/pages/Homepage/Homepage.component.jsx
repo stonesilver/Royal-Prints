f0ec61bbee4398c5ce4134d7d516a08906eb1e47
Ezenwa Chigozie <stonesilver12@gmail.com>
2021-09-07 20:37:35 +0100

    worked on the homepage welcome image to render differnt images for mobile and web views

--- src/pages/Homepage/Homepage.component.jsx
+++ src/pages/Homepage/Homepage.component.jsx
@@ -1,57 +1,38 @@
 import JumpSuit from '../../assets/imgFiles/jumpsuit.jpg';
 import Blazer from '../../assets/imgFiles/blazer.jpg';
 import WelcomeImg1 from '../../assets/imgFiles/large_image.webp';
-// import WomencategoryImg from '../../assets/imgFiles/womenLarge.jpg';
-// import MencategoryImg from '../../assets/imgFiles/menLarge.jpg';
+import WelcomeImg2 from '../../assets/imgFiles/large_image_mobile.webp';
 import bagShopImg from '../../assets/imgFiles/bag_shop.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-elastic-carousel';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import StyleCard from '../../components/StylesCard/StylesCard.component';
 import { pepperDemStyles, trendingFabrics } from '../../assets/carouselData';
+import OninstagramCard from './OnInstagramCard/OninstagramCard';
 import './Homepage.styles.scss';
 
 const Homepage = () => {
+  // I can't get the img element srcSet attribute to work now
+  // I will use this logic to render the homepage welcome image for different screen sizes.
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setScreenWidth(window.innerWidth);
+    });
+  }, []);
+  const src = screenWidth > 768 ? WelcomeImg1 : WelcomeImg2;
+  // This is the code I am using inside the img element but it's not working for now
+  // srcSet={`${WelcomeImg2} 768w, ${WelcomeImg1} 1280w`}
+  // sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
   return (
     <div className='homepage'>
       <div className='welcome'>
         <figure className='figure'>
-          <img src={WelcomeImg1} alt='welcome' className='welcome-img' />
-          {/* {showText && (
-            <figcaption className='caption'>
-              <span>Discover classic styles that will</span>
-              <span>last beyond the season.</span>
-            </figcaption>
-          )} */}
+          <img src={src} alt='welcome' className='welcome-img' />
         </figure>
       </div>
-      {/* <div className='menWomenCategory'>
-        <figure className='womenMenContainer'>
-          <img
-            src={WomencategoryImg}
-            alt='women-category'
-            className='wmCategory-img'
-          />
-          <figcaption className='explore-all'>
-            <Link to='/women' className='explore-all-link'>
-              Explore All
-            </Link>
-          </figcaption>
-        </figure>
-        <figure className='womenMenContainer'>
-          <img
-            src={MencategoryImg}
-            alt='men-category'
-            className='wmCategory-img'
-          />
-          <figcaption className='explore-all'>
-            <Link to='/men' className='explore-all-link'>
-              Explore All
-            </Link>
-          </figcaption>
-        </figure>
-      </div> */}
 
       <div className='jumpsuit-jackets'>
         <div className='jumpsuit-jackets-container'>
@@ -143,6 +124,16 @@ const Homepage = () => {
             <FontAwesomeIcon icon={faAngleRight} />
           </span>
         </Link>
+      </div>
+      <div className='on-instagram'>
+        <div className='on-instagram-header'>
+          <h2>Coronation On Instagram</h2>
+        </div>
+        <div className='on-instagram-container'>
+          {[...Array(9).keys()].map((card, index) => (
+            <OninstagramCard key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Homepage/OnInstagramCard/OninstagramCard.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons';
+import JumpSuit from '../../../assets/imgFiles/jumpsuit.jpg';
+
+const OninstagramCard = () => {
+  return (
+    <div className='on-instagram-card'>
+      <div className='on-instagram-card-img-container'>
+        <img
+          src={JumpSuit}
+          alt='insta'
+          className='on-instagram-card-img-container-img'
+        />
+      </div>
+      <div className='on-instagram-card-hover-state'>
+        <div className='on-instagram-card-hover-state-like-and-comment'>
+          <span>
+            <FontAwesomeIcon icon={faHeart} />
+          </span>
+          <span>25</span>
+        </div>
+        <div className='on-instagram-card-hover-state-like-and-comment'>
+          <span>
+            <FontAwesomeIcon icon={faComment} />
+          </span>
+          <span>12</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OninstagramCard;
