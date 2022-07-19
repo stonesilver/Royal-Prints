138c03ed67df32841297342b164a2ac161d34a14
ezenwa chigozie <stonesilver12@gmail.com>
2022-07-19 18:57:31 +0100

    added scroll to top on category page

--- src/pages/CateoryPage/CateoryPage.component.jsx
+++ src/pages/CateoryPage/CateoryPage.component.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import CategoryButton from './ButtonLink.component';
 import SortAndFilter from './SortAndFilter/SortAndFilter.component';
 import CategoryDisplay from './CategoryDisplay/CategoryDisplay.component';
+import { useScrollToTop } from '../../Hooks/useScrollToTop';
 import './categoryPage.styles.scss';
 
 const CateoryPage = () => {
+  const scrollToTop = useScrollToTop();
+
+  useEffect(() => {
+    scrollToTop();
+  }, [scrollToTop]);
+
   return (
     <div className='category'>
       <CategoryButton />
