af616e8a8b9fb19452516d9bc2b6009f23b31e28
ezenwa chigozie <stonesilver12@gmail.com>
2022-07-31 08:18:33 +0100

    code update

--- src/components/Cart/Cart.component.jsx
+++ src/components/Cart/Cart.component.jsx
@@ -1,10 +1,14 @@
+import { ReactComponent as CloseBtn } from '../../assets/x.svg';
 import './Cart.styles.scss';
 
 const Cart = () => {
   return (
     <div className='cart-modal'>
-      <div className="cart-modal-container">
-          Test cards
+      <div className='cart-modal-container'>
+        <div className='title-and-close-btn'>
+          <p className='cart-title'>MY CART</p>
+          <CloseBtn className='cart-close-btn' />
+        </div>
       </div>
     </div>
   );
