c209e8a0103609db6d7c97859865797b30fe51a1
ezenwa chigozie <stonesilver12@gmail.com>
2022-07-31 18:05:28 +0100

    started cart card component

--- src/components/Cart/Cart.component.jsx
+++ src/components/Cart/Cart.component.jsx
@@ -1,13 +1,29 @@
+import CartCard from './CartCard.component';
 import { ReactComponent as CloseBtn } from '../../assets/x.svg';
+import { ReactComponent as SadMoodIcon } from '../../assets/mood-sad.svg';
 import './Cart.styles.scss';
 
 const Cart = () => {
   return (
     <div className='cart-modal'>
       <div className='cart-modal-container'>
+        {/* header and close btn */}
         <div className='title-and-close-btn'>
           <p className='cart-title'>MY CART</p>
           <CloseBtn className='cart-close-btn' />
+        </div>
+
+        {/* modal content */}
+        <div className='cart-modal-container-content'>
+          <div className='cart-modal-container-content-empty'>
+            <p className='cart-modal-container-content-empty-header'>
+              Your cart is empty
+            </p>
+            <SadMoodIcon className='empty-cart-icon' />
+          </div>
+          {[...Array(5).keys()].map((item) => (
+            <CartCard key={item} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/Cart/CartCard.component.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as TrashIcon } from '../../assets/trash.svg';
+import './CartCard.styles.scss'
+
+const CartCard = () => {
+    return (
+        <div className='cart-card'>
+            Cart Card Test Card
+        </div>
+    )
+}
+
+export default CartCard
