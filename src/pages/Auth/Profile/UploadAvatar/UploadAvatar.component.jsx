f94f7ad1127668cb5db2f2913f30d69507022667
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-22 19:16:48 +0100

    code update

--- src/pages/Auth/Profile/UploadAvatar/UploadAvatar.component.jsx
+++ src/pages/Auth/Profile/UploadAvatar/UploadAvatar.component.jsx
@@ -1,10 +1,17 @@
+import UserAccountItemCard from '../../../../components/UserAccountItemCard/UserAccountItemCard.component';
+import { ReactComponent as PlusIcon } from '../../../../assets/plus.svg';
 import './UploadAvatar.styles.scss';
 
 const UploadAvatar = () => {
   return (
-    <div className='profile-upload-avatar'>
-      <p className='header'>Upload your avatar</p>
-    </div>
+    <UserAccountItemCard title='Upload your avatar'>
+      <div className='profile-upload-avatar'>
+        <div className='profile-upload-avatar-dummy-avatar'>
+          <PlusIcon className='add-icon' />
+          <p className='text'>upload your avatar</p>
+        </div>
+      </div>
+    </UserAccountItemCard>
   );
 };
 
