ab413cf9435019d687a894d4a1b9ce63b3222a34
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-22 06:17:01 +0100

    started profile UI

--- src/pages/Auth/Profile/Profile.component.jsx
+++ src/pages/Auth/Profile/Profile.component.jsx
@@ -1,9 +1,25 @@
-import './Profile.styles.scss'
+import { ReactComponent as UserIcon } from '../../../assets/user.svg';
+import { ReactComponent as KeyIcon } from '../../../assets/svg/key.svg';
+import { ReactComponent as LocationIcon } from '../../../assets/svg/location.svg';
+import './Profile.styles.scss';
+import QuickNavCard from './QuickNavCard';
+
+const quickNav = [
+  { name: 'Profile', icon: <UserIcon />, link: '#profile' },
+  { name: 'Credentials', icon: <KeyIcon />, link: '#credentials' },
+  { name: 'Address', icon: <LocationIcon />, link: '#address' },
+];
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className='user-profile'>
+      <div className='quick-nav-row'>
+        {quickNav.map(({ name, icon, link }) => (
+          <QuickNavCard key={name} name={name} icon={icon} link={link} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
---
+++ src/pages/Auth/Profile/QuickNavCard.jsx
@@ -0,0 +1,5 @@
+const QuickNavCard = ({ name, icon, link }) => {
+  return <div className='quick-nav-card'>QuickNavCard</div>;
+};
+
+export default QuickNavCard;
