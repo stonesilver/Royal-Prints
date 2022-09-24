6cc02c77dbc9945b296e80ec715ce4b8efe3b32c
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-24 06:33:57 +0100

    code update

--- src/pages/Auth/Profile/ProfileDetails/ProfileDetails.component.jsx
+++ src/pages/Auth/Profile/ProfileDetails/ProfileDetails.component.jsx
@@ -20,7 +20,22 @@ const ProfileDetails = () => {
   return (
     <UserAccountItemCard title='Profile'>
       <form onSubmit={handleSubmit} className='profile-details'>
-        
+        <div className='profile-details-first-row'>
+          <div className='profile-details-first-row-columns'>
+            <p className='title'>Account Type</p>
+            <label htmlFor='account-type'>Personal</label>
+            <input type='radio' name='account-type' />
+            <label htmlFor='account-type'>Professional</label>
+            <input type='radio' name='account-type' />
+          </div>
+          <div className='profile-details-first-row-columns'>
+            <p className='title'>Gender</p>
+            <label htmlFor='gender'>Male</label>
+            <input type='radio' name='gender' />
+            <label htmlFor='gender'>Female</label>
+            <input type='radio' name='gender' />
+          </div>
+        </div>
       </form>
     </UserAccountItemCard>
   );
