7b417f16e7cba13f6757f6168309a4b9d01c3d88
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-26 23:23:02 +0100

    code update

--- src/components/base/PhoneNumber/PhoneNumber.component.jsx
+++ src/components/base/PhoneNumber/PhoneNumber.component.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { findFlagUrlByIso3Code } from 'country-flags-svg';
 import './PhoneNumber.styles.scss';
 
 const PhoneNumber = () => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   const toggleDropDown = () => {
-    setOpen((prevS) => !prevS);
+    setOpen(true);
+    ref.current.focus();
   };
 
   const flag = findFlagUrlByIso3Code('NGA');
 
   return (
     <div className='custom-phone-number'>
-      <div className='country-code-select'>
+      <div className='country-code-select' onClick={toggleDropDown}>
         <img src={flag} alt='NGA' className='country-flag-svg' />
       </div>
-      
-      <input type='text' value='+234' />
+
+      <input type='text' value='+234' onChange={() => {}} />
 
       {/* drop-down menu */}
       <div
-        onMouseEnter={toggleDropDown}
         className={`countries-dropdown ${open && 'show-dropdown'}`}
+        ref={ref}
+        onBlur={() => setOpen(false)}
+        tabIndex={0}
       >
         {[0].map((item) => (
           <div key={item} className='countries-dropdown-option'>
             <img src={flag} alt='NGA' className='country-flag-svg' />
-            <p className='country-name'>Nigeria</p>
+            <p className='country-name'>
+              Nigeria <span className='country-code'>+234</span>
+            </p>
           </div>
         ))}
       </div>
