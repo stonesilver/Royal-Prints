0fd44511684d58c82016fc45eaaf517d7193e7e6
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-26 23:03:13 +0100

    code update custom phone number

--- src/components/base/PhoneNumber/PhoneNumber.component.jsx
+++ src/components/base/PhoneNumber/PhoneNumber.component.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import { findFlagUrlByIso3Code } from 'country-flags-svg';
 import './PhoneNumber.styles.scss';
 
 const PhoneNumber = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDropDown = () => {
+    setOpen((prevS) => !prevS);
+  };
+
   const flag = findFlagUrlByIso3Code('NGA');
+
   return (
     <div className='custom-phone-number'>
       <div className='country-code-select'>
         <img src={flag} alt='NGA' className='country-flag-svg' />
       </div>
+      
       <input type='text' value='+234' />
-      <div className="countries-dropdown">
-        
+
+      {/* drop-down menu */}
+      <div
+        onMouseEnter={toggleDropDown}
+        className={`countries-dropdown ${open && 'show-dropdown'}`}
+      >
+        {[0].map((item) => (
+          <div key={item} className='countries-dropdown-option'>
+            <img src={flag} alt='NGA' className='country-flag-svg' />
+            <p className='country-name'>Nigeria</p>
+          </div>
+        ))}
       </div>
     </div>
   );
