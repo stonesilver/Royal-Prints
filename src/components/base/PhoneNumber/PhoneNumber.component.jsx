0ec52b80db0f541d5926bd0e7a290a2df3a9a611
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-26 19:59:04 +0100

    code update custom phone number

--- src/components/base/PhoneNumber/PhoneNumber.component.jsx
+++ src/components/base/PhoneNumber/PhoneNumber.component.jsx
@@ -1,11 +1,19 @@
-import './PhoneNumber.styles.scss'
+import { findFlagUrlByIso3Code } from 'country-flags-svg';
+import './PhoneNumber.styles.scss';
 
 const PhoneNumber = () => {
+  const flag = findFlagUrlByIso3Code('NGA');
   return (
     <div className='custom-phone-number'>
-
+      <div className='country-code-select'>
+        <img src={flag} alt='NGA' className='country-flag-svg' />
+      </div>
+      <input type='text' value='+234' />
+      <div className="countries-dropdown">
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PhoneNumber
+export default PhoneNumber;
