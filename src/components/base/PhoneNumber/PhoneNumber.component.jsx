a6d40269189b7ccb282b1e32e30b61c9f94e02ab
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-28 05:27:08 +0100

    code update

--- src/components/base/PhoneNumber/PhoneNumber.component.jsx
+++ src/components/base/PhoneNumber/PhoneNumber.component.jsx
@@ -1,15 +1,40 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import phoneCode from '../../../assets/countryDailingCode.json';
 import { flagSvg } from '../../../utils/flagSvg';
 import './PhoneNumber.styles.scss';
 
 const PhoneNumber = ({ value, setState, handleChange, Iso2Code }) => {
   const [open, setOpen] = useState(false);
-  const ref = useRef();
+  const containerRef = useRef(null);
+  const itemRef = useRef(null);
+
+  const callBackFunction = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      console.log('Yes!!!');
+    }
+  };
+
+  useEffect(() => {
+    const options = {
+      root: containerRef.current,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const item = itemRef.current;
+
+    const observer = new IntersectionObserver(callBackFunction, options);
+    if (item) observer.observe(item);
+
+    return () => {
+      if (item) observer.unobserve(item);
+    };
+  }, [itemRef]);
 
   const toggleDropDown = () => {
     setOpen(true);
-    ref.current.focus();
+    containerRef.current.focus();
   };
 
   const setCountryCode = (phoneCode, Iso2Code) => {
@@ -41,12 +66,13 @@ const PhoneNumber = ({ value, setState, handleChange, Iso2Code }) => {
       {/* drop-down menu */}
       <div
         className={`countries-dropdown ${open && 'show-dropdown'}`}
-        ref={ref}
+        ref={containerRef}
         onBlur={() => setOpen(false)}
         tabIndex={0}
       >
         {sortedPhoneCode.map(({ country_code, country_en, phone_code }) => (
           <div
+            ref={itemRef}
             key={country_en}
             className='countries-dropdown-option'
             onClick={() => setCountryCode(phone_code, country_code)}
