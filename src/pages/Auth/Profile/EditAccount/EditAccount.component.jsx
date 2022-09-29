502d255381f7cec4ed8acc57443547e179160262
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-29 05:27:06 +0100

    started edit account section

---
+++ src/pages/Auth/Profile/EditAccount/EditAccount.component.jsx
@@ -0,0 +1,10 @@
+import UserAccountItemCard from '../../../../components/UserAccountItemCard/UserAccountItemCard.component';
+import './EditAccount.styles.scss';
+
+const EditAccount = () => {
+  return <UserAccountItemCard title='Edit Account'>
+
+  </UserAccountItemCard>;
+};
+
+export default EditAccount;
--- src/pages/Auth/Profile/Profile.component.jsx
+++ src/pages/Auth/Profile/Profile.component.jsx
@@ -2,6 +2,7 @@ import QuickNav from './QuickNav/QuickNav.component';
 import UploadAvatar from './UploadAvatar/UploadAvatar.component';
 import './Profile.styles.scss';
 import ProfileDetails from './ProfileDetails/ProfileDetails.component';
+import EditAccount from './EditAccount/EditAccount.component';
 
 const Profile = () => {
   return (
@@ -9,6 +10,7 @@ const Profile = () => {
       <QuickNav />
       <UploadAvatar />
       <ProfileDetails />
+      <EditAccount />
     </div>
   );
 };
