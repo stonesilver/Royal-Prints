7c73049079ef8b56705d259f85c382127f72e8a1
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-29 05:32:57 +0100

    code update

--- src/pages/Auth/Profile/EditAccount/EditAccount.component.jsx
+++ src/pages/Auth/Profile/EditAccount/EditAccount.component.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import UserAccountItemCard from '../../../../components/UserAccountItemCard/UserAccountItemCard.component';
 import './EditAccount.styles.scss';
 
 const EditAccount = () => {
-  return <UserAccountItemCard title='Edit Account'>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    oldPassword: '',
+  });
 
-  </UserAccountItemCard>;
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+
+    setFormData((prevS) => ({ ...prevS, [name]: value }));
+  };
+
+  const handleSumit = (event) => {
+    event.preventDefault();
+
+    alert(JSON.stringify(formData));
+  };
+  return (
+    <UserAccountItemCard title='Edit Account'>
+      <form onSubmit={handleSumit} className='edit-account'>
+        
+      </form>
+    </UserAccountItemCard>
+  );
 };
 
 export default EditAccount;
