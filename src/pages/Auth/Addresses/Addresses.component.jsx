3524d5f6e287b0888cf416d158f395d1047ef990
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-30 01:34:44 +0100

    code update

--- src/pages/Auth/Addresses/Addresses.component.jsx
+++ src/pages/Auth/Addresses/Addresses.component.jsx
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import UserAccountItemCard from '../../../components/UserAccountItemCard/UserAccountItemCard.component';
+import Input from '../../../components/base/Input/Input.component';
 import './Addresses.styles.scss';
 
 const Addresses = () => {
+  const [formData, setFormData] = useState({
+    addressName: '',
+    firstName: '',
+    lastName: '',
+    addressLineOne: '',
+    addressLineTwo: '',
+    zip: '',
+    city: '',
+    country: '',
+    deliveryPhoneNumber: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevS) => ({ ...prevS, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(formData));
+  };
+
   return (
     <UserAccountItemCard title='Addresses / New Address'>
-      <div className='user-addresses'>
-        <p className='user-addresses-title user-addresses-important'>
-          ADDRESS NAME (E.G. HOME)
-        </p>
-      </div>
+      <form onSubmit={handleSubmit} className='user-addresses'>
+        <div className='user-addresses-row'>
+          <p className='user-addresses-row-title user-addresses-row-important'>
+            ADDRESS NAME (E.G. HOME)
+          </p>
+          <div className='user-addresses-row-inputs'>
+            <Input
+              type='text'
+              name='addressName'
+              value={formData.addressName}
+              handleChange={handleChange}
+            />
+          </div>
+        </div>
+      </form>
     </UserAccountItemCard>
   );
 };
