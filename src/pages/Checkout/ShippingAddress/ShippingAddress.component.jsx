c43231027500c19ea0295e9445e629005e105784
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-03 17:01:52 +0100

    code update

--- src/pages/Checkout/ShippingAddress/ShippingAddress.component.jsx
+++ src/pages/Checkout/ShippingAddress/ShippingAddress.component.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import UserAccountItemCard from '../../../components/UserAccountItemCard/UserAccountItemCard.component';
 import './ShippingAddress.styles.scss';
 
 const ShippingAddress = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    firstName: '',
+    lastName: '',
+    birthday: { Day: '', Month: '', Year: '' },
+    phone: '',
+    addressLineOne: '',
+    addressLineTwo: '',
+    zip: '',
+    city: '',
+    country: 'Nigeria',
+  });
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setFormData((prevS) => ({ ...prevS, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(formData));
+  };
+
   return (
     <UserAccountItemCard title='Shipping Address'>
-      <div className='checkout-shipping-address'>
-        <div className="checkout-shipping-address-contact"></div>
-        <div className="checkout-shipping-address-address"></div>
-      </div>
+      <form onSubmit={handleSubmit} className='checkout-shipping-address'>
+        <div className='checkout-shipping-address-contact'></div>
+        <div className='checkout-shipping-address-address'></div>
+      </form>
     </UserAccountItemCard>
   );
 };
