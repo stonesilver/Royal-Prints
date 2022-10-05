19875952d2b8845ff3271d23e20bd33c74c7e2d9
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-05 08:00:29 +0100

    code update

--- src/pages/Checkout/CheckoutPayment/TotalAndPayment/TotalAndPayment.component.jsx
+++ src/pages/Checkout/CheckoutPayment/TotalAndPayment/TotalAndPayment.component.jsx
@@ -26,9 +26,43 @@ const TotalAndPayment = () => {
       {/* coupons and order details */}
       <div className='total-and-payment-column-one'>
         <UserAccountItemCard
-          title={<FlexHeader header='Total' value='₦45,254' />}>
+          title={<FlexHeader header='Total' value='₦45,254' />}
+        >
+          <div className='total-and-payment-column-one-total'>
+            <p className='total-and-payment-column-one-total-coupon-label'>
+              Have a coupon ? Enter the code below and click "ok"
+            </p>
+            <div className='total-and-payment-column-one-total-coupon'>
+              <input
+                type='text'
+                name='couponCode'
+                value={state.couponCode}
+                onChange={handleChange}
+                className='total-and-payment-column-one-total-coupon-input'
+              />
+              <button className='total-and-payment-column-one-total-coupon-ok-btn'>
+                ok
+              </button>
+            </div>
             
-          </UserAccountItemCard>
+            <div className='total-and-payment-column-one-order-detail'>
+              <p className='total-and-payment-column-one-order-detail-label'>
+                Sub-total
+              </p>
+              <p className='total-and-payment-column-one-order-detail-value'>
+                ₦45,254
+              </p>
+            </div>
+            <div className='total-and-payment-column-one-order-detail'>
+              <p className='total-and-payment-column-one-order-detail-label'>
+                Shipping fees
+              </p>
+              <p className='total-and-payment-column-one-order-detail-value'>
+                ₦0.0
+              </p>
+            </div>
+          </div>
+        </UserAccountItemCard>
       </div>
       {/* handles payments */}
       <div className='total-and-payment-column-two'></div>
